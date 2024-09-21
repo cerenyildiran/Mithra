1f01b234e58a47a9901adf22bd784311ab04be06
ceren <cereyild@icloud.com>
2024-09-21 02:23:56 +0200

    Check if user exists on the profile page

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ user }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = Cookies.get("accessToken");
+    if (accessToken) {
+      verifyUser(accessToken);
+    } else {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
+  const verifyUser = async (token) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/verifyToken/",
+        { token },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      if (!response.data.user) {
+        navigate("/");
+      }
+    } catch (err) {
+      console.error("Token verification error:", err);
+      navigate("/");
+    }
+  };
   return (
     <div className="profile-page">
       <div className="profile-container">
@@ -12,7 +41,11 @@ const Profile = () => {
             className="profile-pic"
           />
         </div>
-        <h2>username</h2>
+        {user ? (
+          <h2 className="nickname">{user.username}</h2>
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </div>
       <Tabs>
         <TabList>
