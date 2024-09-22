4f6f5869bd135d135e2cffb1d4fcdc35d0f6c3b1
ceren <cereyild@icloud.com>
2024-09-22 12:46:10 +0200

    Navbar CSS updated, background image added for all pages.

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -18,39 +18,69 @@ const Navbar = ({ user }) => {
     setDropdownVisible(!dropdownVisible);
   };
   return (
-    <nav className="navbar">
-      <div className="navbar-links">
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-      </div>
-      <div className="navbar-logo">
-        <Link to="/">
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
           <img src="/img/logo.png" alt="Logo" style={{ height: "60px" }} />
         </Link>
-      </div>
-      <div className="navbar-user">
-        {user ? (
-          <>
-            <div className="profile-menu" >
-            <Link to="/profile">
-          <FaUserCircle className="profile-icon" size={25} />
-        </Link>
-              <span className="user-greeting">Welcome, {user.username}</span>
-              <FaChevronDown className="dropdown-icon" onClick={toggleDropdown} style={{ position: 'relative' }}/>
-              {dropdownVisible && (
-                <div className="dropdown-menu" >
-                  <button onClick={handleLogout} className="dropdown-item">Log out</button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            {user ? (
+              <li className="nav-item dropdown">
+                <div className="d-flex align-items-center">
+                  <Link to="/profile" className="nav-link">
+                    <FaUserCircle className="profile-icon" size={25} />
+                  </Link>
+                  <span className="user-greeting">Welcome, {user.username}</span>
+                  <FaChevronDown
+                    className="dropdown-icon ms-2"
+                    onClick={toggleDropdown}
+                    style={{ cursor: "pointer" }}
+                  />
                 </div>
-              )}
-            </div>
-          </>
-        ) : (
-          <>
-            <Link to="/login" className="nav-link">Log in</Link>
-            <Link to="/register" className="nav-link">Register</Link>
-          </>
-        )}
+                {dropdownVisible && (
+                  <ul className="dropdown-menu dropdown-menu-end" style={{ position: "absolute" }}>
+                    <li>
+                      <button onClick={handleLogout} className="dropdown-item">
+                        Log out
+                      </button>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    Log in
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/register" className="nav-link">
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </nav>
   );
